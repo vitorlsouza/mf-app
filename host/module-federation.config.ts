ff30f3bf6d20c295f66a4fbc315b8e45dd12fdaf
Vitor <vitorlopesdesouza@gmail.com>
2025-06-26 00:22:29 -0400

    feat: deploy remote

--- host/module-federation.config.ts
+++ host/module-federation.config.ts
@@ -3,7 +3,7 @@ export const mfConfig = {
   filename: "remoteEntry.js",
   remotes: {
     remote:
-      "remote@https://vitor-lopes-de-souza-10-remote-mf-app-vitorlsouza-d03e9e851-ze.zephyrcloud.app/remoteEntry.js",
+      "remote@https://vitor-lopes-de-souza-11-remote-mf-app-vitorlsouza-d03e9e851-ze.zephyrcloud.app/remoteEntry.js",
   },
   exposes: {},
   shared: ["react", "react-dom"],
